5ef31e4f0ead3c1f35736ba035471a828596e5cd
mahiranand <mahiranand08@gmail.com>
2023-12-30 13:14:16 +0530

    minor update

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,18 +1,30 @@
-import PropTypes from 'prop-types';
-export const Dropdown = ({ set, data}) => {
+import PropTypes from "prop-types";
+import { useState } from "react";
+export const Dropdown = ({ set, data }) => {
   Dropdown.propTypes = {
-    set : PropTypes.func.isRequired,
-    data : PropTypes.object.isRequired
-  }
+    set: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired,
+  };
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+    set(event.target.value);
+  };
   return (
     <div className="dropdown">
       <select
         className={data.className}
         name={data.name}
-        onChange={(e) => set(e.target.value)}
+        value={selectedOption}
+        onChange={handleDropdownChange}
       >
         {data.optionsText.map((text, i) => {
-          return <option value={data.optionsValue[i]} key = {i}>{text}</option>;
+          return (
+            <option value={data.optionsValue[i]} key={i}>
+              {text}
+            </option>
+          );
         })}
       </select>
     </div>
